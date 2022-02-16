aa781e87a89b84287ec04a0402a8711287fe9a4c
niemczus <niemczus@gmail.com>
2022-02-16 15:14:09 +0100

    UI app created

--- Brain Exoneration/ViewController.swift
+++ Brain Exoneration/ViewController.swift
@@ -7,13 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var notes = ["hello", "goodbye"]
+    
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+          super.viewDidLoad()
+        
+        tableView.dataSource = self
+          
+      }
+
+    
+    @IBAction func didTapAdd(_ sender: Any) {
+        print("add")
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let note = notes[indexPath.row]
+        
+        let cell = UITableViewCell()
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = note
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    
+    
+    
 }
+
+
 
